88079d84b7d7700e428f43f79bbafec6de0d827b
Jericho Gamboa <jerichogamboa111@gmail.com>
2026-05-20 23:33:14 +0800

    make site fully responsive for all devices

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <Link to="/" className="navbar-brand">
-        🍽️ <span>Saveur</span>
+        🍽️ <span>Home</span>
       </Link>
 
       <div className="navbar-links">
